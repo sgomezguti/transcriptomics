8fe7328b41da56d9c09b5246de31ddef86d501a3
sgomezguti <sgomezguti@gmail.com>
2024-05-03 11:16:24 -0400

    Added path to P. maydis raw data

--- headers.py
+++ headers.py
@@ -3,5 +3,5 @@ Headers
 Change the headers in a Pacbio fasta file so each identifier is unique for each individual
 read.
 ================================================================================================="""
-pacbio=""
+pacbio="/scratch/negishi/sgomezgu/phylachora_genome"
 headers={}
