88a3036cf69ced8db5408debc9067869df2aca3f
sgomezgu <sgomezgu@purdue.edu>
2024-05-02 20:02:44 -0400

    Testing of first commit

---
+++ headers.py
@@ -0,0 +1,7 @@
+"""=================================================================================================
+Headers
+Change the headers in a Pacbio fasta file so each identifier is unique for each individual
+read.
+================================================================================================="""
+pacbio=""
+headers={}
